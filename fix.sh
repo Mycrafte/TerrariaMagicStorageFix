1e70d1d9f0b268fadfeb53e5466c6a35415d7442
keeptpa <keeptpa@qq.com>
2019-08-25 22:15:00 +0800

    主文件，其实核心是wget

---
+++ fix.sh
@@ -0,0 +1,5 @@
+echo 这里能看见Github的服务器，不错。 
+sleep 2
+wget https://github.com/keeptpa/TerrariaMagicStorageFix/releases/download/Set1/assestlib.so -O ./lib64/libMonoPosixHelper.so
+clear
+echo 暂时这样，试试去吧。有什么事再来找我
