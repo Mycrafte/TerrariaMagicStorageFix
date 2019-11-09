6ea4adafb58c63e0639ab2659bf3a59c751e43aa
keeptpa <keeptpa@qq.com>
2019-11-09 10:14:22 +0800

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,5 +1,5 @@
-echo 这里能看见Github的服务器，不错。 
+echo Trying dowload from github
 sleep 2
 wget https://github.com/keeptpa/TerrariaMagicStorageFix/releases/download/Set1/assestlib.so -O ./lib64/libMonoPosixHelper.so
 clear
-echo 暂时这样，试试去吧。有什么事再来找我
+echo Done.
