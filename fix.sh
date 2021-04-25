9e76d369c4ff363086a9b8ba5cc6d34c1f008d91
keeptpa <keeptpa@qq.com>
2021-04-25 17:06:20 +0800

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,5 +1,4 @@
 echo Trying dowload from github
-sleep 2
 wget https://github.com/keeptpa/TerrariaMagicStorageFix/releases/download/Set1/assestlib.so -O ./lib64/libMonoPosixHelper.so
 clear
 echo Done.
